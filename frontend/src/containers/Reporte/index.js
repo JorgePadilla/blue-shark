f60f977229869dc23b3b00a12954603e5cf28c5f
JorgePadilla <jorgep4dill4@gmail.com>
2021-03-19 03:53:44 -0600

    add report container

---
+++ frontend/src/containers/Reporte/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Alert } from 'antd';
+
+const Reporte = () => {
+    return <Alert message="404 Error" description="Reporte." type="error" showIcon />;
+}
+    
+export default Reporte;
