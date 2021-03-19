572c89a6f7bce4b9a3fafb8891661d5528f33d08
JorgePadilla <jorgep4dill4@gmail.com>
2021-03-19 01:32:41 -0600

    added routes

--- frontend/src/routes/mainRoutes.js
+++ frontend/src/routes/mainRoutes.js
@@ -1,6 +1,8 @@
 import Board from 'containers/Board';
 import SignIn from 'containers/SignIn';
 import NotFound from 'containers/NotFound';
+import Agenda from 'containers/Agenda';
+import Paciente from 'containers/Paciente';
 
 const mainRoutes = [
   {
@@ -16,6 +18,30 @@ const mainRoutes = [
     path: '/signin',
     name: 'Sign In',
     icon: 'login',
+    component: SignIn,
+  },
+  {
+    path: '/paciente',
+    name: 'Pacientes',
+    icon: 'user',
+    component: Paciente,
+  },
+  {
+    path: '/agenda',
+    name: 'Agenda',
+    icon: 'calendar',
+    component: Agenda,
+  },
+  {
+    path: '/paciente',
+    name: 'Facturación',
+    icon: 'form',
+    component: SignIn,
+  },
+  {
+    path: '/paciente',
+    name: 'Reportes',
+    icon: 'bar-chart',
     component: SignIn,
   },
   {
